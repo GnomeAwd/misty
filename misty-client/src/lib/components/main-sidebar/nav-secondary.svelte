73da3826899ecab2a57f6aa02623675c9f70a6c4
GnomeAwd <srenjoy.saha12@gmail.com>
2025-08-21 18:28:36 +0530

    Add rescan functionality to sidebar and remove scan button from home page

--- misty-client/src/lib/components/main-sidebar/nav-secondary.svelte
+++ misty-client/src/lib/components/main-sidebar/nav-secondary.svelte
@@ -3,7 +3,9 @@
 	import type { WithoutChildren } from '$lib/utils.js';
 	import type { ComponentProps } from 'svelte';
 	import type { Icon } from '@tabler/icons-svelte';
-	import { page } from '$app/stores';
+	import RotateClockwise from '@tabler/icons-svelte/icons/rotate-clockwise';
+	import { scan } from '$lib/api/scan';
+
 
 	let {
 		items,
@@ -11,6 +13,21 @@
 	}: { items: { title: string; url: string; icon: Icon }[] } & WithoutChildren<
 		ComponentProps<typeof Sidebar.Group>
 	> = $props();
+
+
+	let res = $state(null);
+
+	const handleScan = async () => {
+		const result = await scan();
+		res = result;
+		if (result.success === 200) {
+			// alert('Scan completed successfully!');
+			//reload page
+			window.location.reload();
+		} else {
+			// alert('Scan failed. Please try again.');
+		}
+	};
 
 </script>
 
@@ -30,6 +47,12 @@
 				</Sidebar.MenuButton>
 			  </Sidebar.MenuItem>
 			{/each}
+			<Sidebar.MenuItem>
+				<Sidebar.MenuButton onclick={handleScan} size="lg" class="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground data-[state=open]:px-4">
+				  <RotateClockwise class="h-8 w-8 scale-150 mx-2" />
+				  <span class="text-sm">Rescan</span>
+				</Sidebar.MenuButton>
+			</Sidebar.MenuItem>
 		</Sidebar.Menu>
 	</Sidebar.GroupContent>
 </Sidebar.Group>
--- misty-client/src/routes/+page.svelte
+++ misty-client/src/routes/+page.svelte
@@ -1,18 +1,7 @@
 <script>
 	import Button from '$lib/components/ui/button/button.svelte';
-	import { scan } from '$lib/api/scan';
 
-	let res = $state(null);
-
-	const handleScan = async () => {
-		const result = await scan();
-		res = result;
-	};
+	
 </script>
 
 <span>Home</span>
-<Button onclick={handleScan}>Scan</Button>
-
-{#if res}
-	<pre>{JSON.stringify(res, null, 2)}</pre>
-{/if}
