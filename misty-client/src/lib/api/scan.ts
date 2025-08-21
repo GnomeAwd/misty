f2ffd2d8b8fed28c03f328760539bc96849649f9
GnomeAwd <srenjoy.saha12@gmail.com>
2025-08-21 18:28:19 +0530

    Update scan function to use dynamic base URL for API requests based on environment

--- misty-client/src/lib/api/scan.ts
+++ misty-client/src/lib/api/scan.ts
@@ -1,4 +1,6 @@
+const baseUrl = process.env.NODE_ENV === "production" ? "" : "http://localhost:3000";
+
 export const scan = async () => {
-  const response = await fetch("/api/scan");
+  const response = await fetch(baseUrl+"/api/scan");
   return response.json();
 };
