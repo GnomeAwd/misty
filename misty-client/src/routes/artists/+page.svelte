fba04e0c564d380027101b83e3860cd284f8935c
GnomeAwd <srenjoy.saha12@gmail.com>
2025-08-24 01:01:25 +0530

    Add artist page with search functionality and data loading

--- misty-client/src/routes/artists/+page.svelte
+++ misty-client/src/routes/artists/+page.svelte
@@ -1,2 +1,14 @@
-<span>Artists</span>
+<script>
+	import { Button } from "$lib/components/ui/button";
+	import Input from "$lib/components/ui/input/input.svelte";
+
+</script>
+<div class="flex flex-col items-start justify-start w-full h-[90.5vh] p-4 bg-background">
+    <div class="w-full flex items-center justify-between gap-2">
+        <Input placeholder="Search" />
+        <Button variant="outline">Search</Button>
+        <Button variant="outline">Filters</Button>
+    </div>
+    <div>Grid</div>
+</div>
 
