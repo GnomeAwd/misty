4416641e4baae543e9840f755b4556fa1f0c40a0
GnomeAwd <srenjoy.saha12@gmail.com>
2025-08-23 21:20:46 +0530

    Add song retrieval functions for fetching by ID, artist ID, and album ID

--- misty-server/src/getters/songs.ts
+++ misty-server/src/getters/songs.ts
@@ -1,7 +1,24 @@
 import { db } from "../db";
 import { songs } from "../db/schema";
+import { eq } from "drizzle-orm";
+
 
 export const getAllSongs = async () => {
   const songsData = await db.select().from(songs)
+  return songsData
+}
+
+export const getSongById = async (id: number) => {
+  const songData = await db.select().from(songs).where(eq(songs.id, id)).limit(1)
+  return songData[0]
+}
+
+export const getAllSongsByArtistId = async (artistId: number) => {
+  const songsData = await db.select().from(songs).where(eq(songs.artistId, artistId))
+  return songsData
+}
+
+export const getAllSongsByAlbumId = async (albumId: number) => {
+  const songsData = await db.select().from(songs).where(eq(songs.albumId, albumId))
   return songsData
 }
