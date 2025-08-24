325d97ef4d632355c69721cb1d287764beca3a10
GnomeAwd <srenjoy.saha12@gmail.com>
2025-08-24 05:55:15 +0530

    Add artist grid card component and implement artist listing with toggle view

---
+++ misty-client/src/lib/components/artists/artist-grid-card.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import Button from "../ui/button/button.svelte";
+
+
+ interface Artist {
+   artistBackgroundUrl: string
+   artistBannerUrl: string
+   artistHdMusicLogoUrl: string
+   avatarUrl: string
+   bio: string
+   createdAt: string
+   folderPath: string
+   id: number
+   musicbrainzId: string
+   name: string
+   updatedAt: string
+ }
+
+ let {artist}: {artist: Artist} = $props();
+
+</script>
+
+<Button variant="outline" class="h-80 w-64 flex flex-col items-center justify-between overflow-hidden gap-2 p-4 bg-background hover:bg-muted/80 dark:bg-background dark:hover:bg-muted/20">
+    <img src={artist.avatarUrl} alt={artist.name} class="object-cover rounded-full w-full aspect-square" />
+    <img src={artist.artistHdMusicLogoUrl} alt={artist.name} class="object-contain h-12 w-full" />
+
+</Button>
--- misty-client/src/routes/artists/+page.svelte
+++ misty-client/src/routes/artists/+page.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
+	import ArtistGridCard from "$lib/components/artists/artist-grid-card.svelte";
 	import { Button } from "$lib/components/ui/button";
 	import Input from "$lib/components/ui/input/input.svelte";
+	import * as  ToggleGroup from "$lib/components/ui/toggle-group/index.js";
+	import GridPattern from "@tabler/icons-svelte/icons/grid-pattern";
 	import type { PageData } from "./$types";
+	import ListDetails from "@tabler/icons-svelte/icons/list-details";
     let { data }: { data: PageData } = $props();
     const artists = $derived(data.artists);
 
@@ -9,12 +13,22 @@
         console.log(artists);
     });
 </script>
-<div class="flex flex-col items-start justify-start w-full h-[90.5vh] p-4 bg-background">
+<div class="flex flex-col items-start justify-start w-full h-[90.5vh] p-4 bg-background gap-4">
     <div class="w-full flex items-center justify-between gap-2">
-        <Input placeholder="Search" />
-        <Button variant="outline">Search</Button>
-        <Button variant="outline">Filters</Button>
+        <Button variant="outline" class="bg-background hover:bg-muted/20 dark:bg-background dark:hover:bg-muted/20">Filters</Button>
+        <ToggleGroup.Root variant="outline" type="single" value="grid">
+            <ToggleGroup.Item value="grid" class="p-2">
+                <GridPattern class="h-4 w-4" />
+            </ToggleGroup.Item>
+            <ToggleGroup.Item value="list" class="p-2">
+                <ListDetails class="h-4 w-4" />
+            </ToggleGroup.Item>
+        </ToggleGroup.Root>
     </div>
-    <div>Grid</div>
+    <div class="flex items-start justify-start flex-wrap gap-4 w-full">
+        {#each artists as artist}
+            <ArtistGridCard {artist} />
+        {/each}
+    </div>
 </div>
 
