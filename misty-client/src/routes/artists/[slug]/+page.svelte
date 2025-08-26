575cbf00ca01c2f128a56fad19103da699d498f3
GnomeAwd <srenjoy.saha12@gmail.com>
2025-08-26 21:12:26 +0530

    Enhance artist page layout and functionality by adding song data and total duration calculation

--- misty-client/src/routes/artists/[slug]/+page.svelte
+++ misty-client/src/routes/artists/[slug]/+page.svelte
@@ -1,16 +1,63 @@
 <script lang="ts">
+	import { Button } from "$lib/components/ui/button";
+	import PlayerPlay from "@tabler/icons-svelte/icons/player-play";
 	import type { PageData } from "./$types";
+	import ArrowsShuffle_2 from "@tabler/icons-svelte/icons/arrows-shuffle-2";
+	import Heart from "@tabler/icons-svelte/icons/heart";
+	import ArtistAlbumAccordion from "$lib/components/artists/artist-album-accordion.svelte";
+	import Circle from "@tabler/icons-svelte/icons/circle";
+	import Separator from "$lib/components/ui/separator/separator.svelte";
 
 
     let { data }: { data: PageData } = $props();
 
-    const { artist, albums } = data;
+    const { artist, albums, songs } = data;
 
-    $effect(() => {
-        console.log(artist, albums);
-    });
+    let totalDuration = $state(0)
 
+
+	const formatSecondCount = (seconds: number) => {
+		let hour = Math.floor(seconds / 3600);
+		let minute = Math.floor((seconds % 3600) / 60);
+		let second = seconds % 60;
+
+		return `${hour === 0 ? '' : hour + 'hr '}${minute}min ${second}sec`;
+	};
+
+    $inspect(artist)
 </script>
-<span>
+<div class="flex flex-col items-start justify-start w-full h-[90.5vh] bg-muted dark:bg-background gap-4">
+    <div class="w-full relative h-64 overflow-hidden">
+        <div class="w-full h-full bg-gradient-to-b from-transparent via-muted/50 to-muted dark:to-background dark:via-background/50 absolute bottom-0 left-0"></div>
+        <img src={artist.artistBannerUrl} alt="artist banner" class="w-full object-cover h-full" />
+    </div>
+        <div class="w-full relative p-12">
+            <div class="flex w-full items-center justify-start gap-4 absolute -top-8 left-0 px-12">
+                <Button class="rounded-full h-16 w-16">
+                    <PlayerPlay class="scale-150 fill-background" />
+                </Button>
+                <Button variant="outline" class="rounded-full h-12 w-12">
+                    <ArrowsShuffle_2/>
+                </Button>
+                <Button variant="outline" class="rounded-full h-12 w-12">
+                    <Heart/>
+                </Button>
+            </div>
+            <div class="w-full grid grid-cols-3 gap-12">
+                <div class="col-span-1 flex flex-col items-start justify-start gap-2">
+                    <span class="text-2xl font-bold">{artist.name}</span>
+                    <div class="flex gap-2 items-center justify-start">
+                        <span class="text-sm text-muted-foreground">{albums.length} Albums</span>
+                        <Circle class="h-2 w-2 fill-muted dark:fill-background" />
+                        <span class="text-sm text-muted-foreground">{formatSecondCount(totalDuration)}</span>
+                    </div>
+                    <Separator />
+                    <span class="text-sm text-muted-foreground text-justify">{artist.bio}</span>
 
-</span>
+                </div>
+                <div class="col-span-2 flex items-center justify-end gap-2">
+                    <ArtistAlbumAccordion albums={albums} songs={songs} bind:totalDuration={totalDuration} />
+                </div>
+            </div>
+        </div>  
+</div>  
