e7c07b2e09d808e3dde0e9855dbceeea1d789cdf
GnomeAwd <srenjoy.saha12@gmail.com>
2025-09-02 19:42:04 +0530

    Add artist list card component for displaying artist details

---
+++ misty-client/src/lib/components/artists/artist-list-card.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import Button from "../ui/button/button.svelte";
+
+
+ interface Artist {
+   artistBackgroundUrl: string
+   artistBannerUrl: string
+   artistHdMusicLogoUrl: string
+   avatarUrl: string
+   bio: string
+   createdAt: string
+   folderPath: string
+   id: number
+   musicbrainzId: string
+   name: string
+   updatedAt: string
+ }
+
+ let {artist}: {artist: Artist} = $props();
+
+</script>
+
+<Button href={`/artists/${artist.id}`} variant="ghost" class="h-15 w-full flex items-center justify-between hover:bg-primary/20 hover:text-foreground py-2 px-2 gap-4">
+    <img src={artist.avatarUrl} alt={artist.name} class="object-cover w-12 h-12 rounded-md" />
+    <div class="flex flex-col items-start justify-start gap-1 w-full">
+        <span class="w-full truncate text-left text-sm font-medium">{artist.name}</span>
+    </div>  
+</Button>
